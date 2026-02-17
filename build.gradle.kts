318f754e11701ea8dcb32b9538dcbedee8c1d851
Takayuki Ueno <mail@kichi2004.jp>
2026-02-17 21:48:12 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "jp.kichi2004"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:6.1.3")
+    testImplementation("io.kotest:kotest-assertions-core:6.1.3")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
